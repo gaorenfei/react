9cc07909a7d5faa92e8adea46721f861e0c6c511
高任飞 <18767825129@163.com>
2018-02-02 17:38:39 +0800

    路由按需加载

--- myapp/src/route/index.js
+++ myapp/src/route/index.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { Route, Redirect, IndexRedirect } from 'react-router';
+// import React from 'react';
+// import { Route, Redirect, IndexRedirect } from 'react-router';
 
 
-import  App from '../App'
-import  Contents from '../components/content'
-import  Page1 from '../components/page1'
-import  Page2 from '../components/page2'
+// import  App from '../App'
+// import  Contents from '../components/content'
+// import  Page1 from '../components/page1'
+// import  Page2 from '../components/page2'
 
 
-// 可以在这里写一些在路由即将被改变时触发的函数
-// 可以用参数replace改变接下来的路由地址
-const requireAuth = (nextState, replace) => {
-    console.log('进入/content/page2')
-}
+// // 可以在这里写一些在路由即将被改变时触发的函数
+// // 可以用参数replace改变接下来的路由地址
+// const requireAuth = (nextState, replace) => {
+//     console.log('进入/content/page2')
+// }
 
-export default (
-    <Route path="/">
-        <IndexRedirect to="/login" />
-        <Route path="/login" component={App}/>
-        <Route path="/content" component={Contents}>
-            <IndexRedirect to="/content/page1" />
-            <Route path="/content/page1" component={Page1}/>
-            <Route path="/content/page2" component={Page2} onEnter={requireAuth} />
-        </Route>
-        <Redirect from='*' to='/'  />
-    </Route>
-);
+// export default (
+//     <Route path="/">
+//         <IndexRedirect to="/login" />
+//         <Route path="/login" component={App}/>
+//         <Route path="/content" component={Contents}>
+//             <IndexRedirect to="/content/page1" />
+//             <Route path="/content/page1" component={Page1}/>
+//             <Route path="/content/page2" component={Page2} onEnter={requireAuth} />
+//         </Route>
+//         <Redirect from='*' to='/'  />
+//     </Route>
+// );
 
 //按需加载
-// export default {
-//     path: '/',
-//     indexRoute: {
-//       getComponent(nextState, cb) {
-//         require.ensure([], (require) => {
-//           cb(null, require('../App').default)
-//         }, 'login')
-//       }
-//     },
-//     childRoutes: [
-//       require('./content').default,
-//       require('./redirect').default
-//     ]
-// }
+export default {
+    path: '/',
+    indexRoute: {
+      getComponent(nextState, cb) {
+        require.ensure([], (require) => {
+          cb(null, require('../App').default)
+        }, 'login')
+      }
+    },
+    childRoutes: [
+      require('./content').default,
+      require('./redirect').default
+    ]
+}
